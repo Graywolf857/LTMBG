d407948537a6f8abfdea6963f7223fd196e18fb7
Graywolf857 <graysonjones857@gmail.com>
2023-11-03 19:32:57 -0700

    Blink Dagger working
    Signed-off-by: Graywolf857 <graysonjones857@gmail.com>

--- src/main/java/net/graywolf857/learningmod/item/custom/BlinkDagger.java
+++ src/main/java/net/graywolf857/learningmod/item/custom/BlinkDagger.java
@@ -1,16 +1,13 @@
 package net.graywolf857.learningmod.item.custom;
 
-import net.minecraft.core.Direction;
+
 import net.minecraft.world.InteractionHand;
 import net.minecraft.world.InteractionResultHolder;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.Level;
-import net.minecraft.world.phys.Vec3;
 
-import java.util.HashMap;
-import java.util.Map;
 
 public class BlinkDagger extends Item {
 
@@ -42,9 +39,3 @@ public class BlinkDagger extends Item {
     }
 
 }
-
-
-
-
-
-
--- src/main/java/net/graywolf857/learningmod/item/custom/TNTWand.java
+++ src/main/java/net/graywolf857/learningmod/item/custom/TNTWand.java
@@ -1,18 +1,9 @@
 package net.graywolf857.learningmod.item.custom;
 
-import net.minecraft.client.resources.language.I18n;
-import net.minecraft.core.BlockPos;
-import net.minecraft.network.chat.Component;
-import net.minecraft.world.InteractionResult;
-import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.Item;
-import net.minecraft.world.item.context.UseOnContext;
-import net.minecraft.world.level.block.Block;
-import net.minecraft.world.level.block.Blocks;
-import net.minecraft.world.level.block.state.BlockState;
+
 
 public class TNTWand extends Item {
-
     public TNTWand(Properties pProperties) {
         super(pProperties);
     }
